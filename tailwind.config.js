49426198bb8a26d1e2a75356989dc337945a1d3d
Finnder <mcguirefinneganftw@gmail.com>
2022-06-07 10:51:40 -0400

    Added stuff

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,js,tsx}"],
+  theme: {
+      extend: {
+        colors: {
+            primary: '#202225',
+            secondary: '#5865f2'
+        }
+      },
+  },
+  plugins: [],
+}
